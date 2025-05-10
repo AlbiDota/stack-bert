b78110a5929955dde346a6395f8dbf644ae51388
AlbiDota <alisaele1@gmail.com>
2025-05-10 17:57:16 +0200

    Initial commit - PERN starts now!

---
+++ server/database.js
@@ -0,0 +1,38 @@
+const { Pool } = require("pg");
+
+const pool = new Pool({
+    user: "postgres",
+    password: "2424",
+    host: "localhost",
+    port: 5432,
+    database: "stackbertdb"
+})
+
+
+/* 
+pool.query("CREATE DATABASE stackbertdb;")
+.then((Response) => {
+    console.log("Database created");
+    console.log(Response);
+})
+.catch((err) => {
+    console.warn(err);
+});
+
+
+const createTblQry = `CREATE TABLE users (
+    user_id serial PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    password VARCHAR(50) UNIQUE NOT NULL);`;
+
+pool.query(createTblQry)
+.then((Response) => {
+    console.log("Table created");
+    console.log(Response);
+})
+.catch((err) => {
+    console.warn(err);
+});
+*/
+
+module.exports = pool
